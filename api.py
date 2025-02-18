af079d00678341543b0fca95e1ac77229ae6ffc9
Casper2411 <ccaassppeerr2411@gmail.com>
2025-02-18 11:37:06 +0100

    implemented .env for api-key

--- api.py
+++ api.py
@@ -1,10 +1,30 @@
 # LLM API interaction
 
+## loading secrets
+from dotenv import load_dotenv
+import os
+
+### Load environment variables from .env
+load_dotenv()
+
+### Access API key
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+
+
 ## Claude API
 import anthropic # type: ignore
 
+
+### checking if the API key is correctly loaded
+
+if anthropic_api_key:
+    print("API Key Loaded Successfully!")
+else:
+    raise ValueError("API Key Not Found! Make sure to set it in the .env file.")
+
+
 def ClaudeAPI(input):
-    client = anthropic.Anthropic()
+    client = anthropic.Anthropic(api_key=anthropic_api_key)
 
     message = client.messages.create(
         model="claude-3-5-sonnet-20241022",
