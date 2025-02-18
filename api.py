bcc1959c1176738b3bf496a86f44b5b6361cc047
Casper2411 <ccaassppeerr2411@gmail.com>
2025-02-18 11:18:22 +0100

    created .gitignore and made an example API call

--- api.py
+++ api.py
@@ -1,1 +1,29 @@
 # LLM API interaction
+
+## Claude API
+import anthropic # type: ignore
+
+def ClaudeAPI(input):
+    client = anthropic.Anthropic()
+
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20241022",
+        max_tokens=1000,
+        temperature=0,
+        system="You are a world-class programmer. Respond only with code.",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": input
+                    }
+                ]
+            }
+        ]
+    )
+    print(message.content)
+
+
+ClaudeAPI("Make a function that finds the square root of a given number, and has error handling.")
