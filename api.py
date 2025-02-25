0a7201e72b3bccc7a6386a5528ce3e5ab8c294d6
Casper2411 <ccaassppeerr2411@gmail.com>
2025-02-25 19:19:30 +0100

    push of code generation api

--- api.py
+++ api.py
@@ -81,7 +81,7 @@ def ClaudeAPI(input_list, assistant_settings=None):
     return error_handling_wrapper(
         client.messages.create,
         model="claude-3-5-sonnet-20241022",
-        max_tokens=1000,
+        max_tokens=8192,
         temperature=0,
         system=f"You are a world-class programmer. {assistant_settings}",
         messages= input_list
@@ -107,7 +107,34 @@ def CreateComplianceReportArchitecture(input_list, chosen_API=APIChoice.CLAUDE):
     returns: an Compliance report from Claude(A list with a TextBlock inside it), or None if error occurs
     """
     if chosen_API == APIChoice.CLAUDE:
-        return ClaudeAPI(input_list, """I will give you some code files, where I will start by giving the relative path, and then the code. Each file starts with '### START FILE: <filename> ###' and ends with '### END FILE: <filename> ###'. You should now decide if these files and their placement in the folders have compliance of the code architecture "Onion", in accordance to if it upholds these standards. You shall return a list of what is wrong according to the "Onion" architecture with the code given to you, and return nothing else than the list, where each point in the list corresponds to one single violation of the Onion architecture. You should be focusing on dependency flow, layer responsibilities, and domain isolation. Your analysis will be precise and actionable, highlighting only genuine architectural violations, and naming the exact files involved, and the specific principle being violated. If no violations are found, return "No vioolations found." """)
+        return ClaudeAPI(input_list, """I will give you some code files, where I will start by giving the relative path, and then the code. Each file starts with '### START FILE: <filename> ###' and ends with '### END FILE: <filename> ###'. You should now decide if these files and their placement in the folders have compliance of the code architecture "Onion", in accordance to if it upholds these standards. You shall return a list of what is wrong according to the "Onion" architecture with the code given to you, and return nothing else than the list, where each point in the list corresponds to one single violation of the Onion architecture. You should be focusing on dependency flow, layer responsibilities, and domain isolation. Your analysis will be precise and actionable, highlighting only genuine architectural violations, and naming the exact files involved, and the specific principle being violated. If no violations are found, return "No violations found." """)
+    elif chosen_API == APIChoice.CHATGPT:
+        raise NotImplementedError("ChatGPT support is not implemented yet.")
+    
+    else:
+        raise ValueError(f"Invalid API choice: {chosen_API}")
+    
+
+def CreateCodeArchitectureFix(input_list, chosen_API=APIChoice.CLAUDE):
+    """
+    This function is for creating the Compliance report
+
+    input:
+        input_list: List of Json file objects, that is a previous conversation with an AI, that has user and assistant text messages.
+        chosen_API: ENum, which API should be used to create this. Default is Claude.
+
+    returns: The corrected code from Claude(A list with a TextBlock inside it), or None if error occurs
+    """
+    if chosen_API == APIChoice.CLAUDE:
+        return ClaudeAPI(input_list, """I earlier gave you some code files, where I started by giving the relative path, and then the code. 
+                         You then gave feedback on what architecture violations existed in accordance to the "Onion" Architecture.
+                         You shall now return a corrected version of the code, where the points, that was asked to be fixed by the user, is given back to you. 
+                         You shall NOT change anything in the code, that is not part of one of the points specified by the user.
+                         You shall return all the code files given to you originally, also the ones that you have not made any architecturally changes to, but also the ones you have made architecturally changes to.
+                         You shall also return the relative paths of all the files, both the ones you have changed the location of due to architectural changes, and the ones you haven't.
+                         You shall start by giving the relative paths, and then the code.
+                         You shall NOT return anything other than specified here, so no explaination of why this is done, or anything like this.
+                         If no points are given by the user, return "No changes to the code" """)
     elif chosen_API == APIChoice.CHATGPT:
         raise NotImplementedError("ChatGPT support is not implemented yet.")
     
